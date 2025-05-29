426121044996ae03892dfa6153ed6ce9049e6bda
ailbay19 <79639413+ailbay19@users.noreply.github.com>
2025-05-29 23:48:52 +0300

    fix: add blueprint

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
 from medsam2 import bp as ai_bp
+from MEDSAM3D import bp as vd_bp
 
 def create_app():
     app = Flask(__name__)
     app.register_blueprint(ai_bp)
+    app.register_blueprint(vd_bp)
 
     return app
 
